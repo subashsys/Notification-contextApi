228fd7c4c1fbcd7ffac53c21e2d653b0b11914dd
Subash Ghimire <hellosubash7@gmail.com>
2026-07-14 10:40:02 +0545

    Apply Notification wrapper

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { NotificationProvider } from "@/context/NotificationProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,7 +29,10 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-           {children}
+        <NotificationProvider>
+          {children}
+        </NotificationProvider>
+           
        
         </body>
     </html>
